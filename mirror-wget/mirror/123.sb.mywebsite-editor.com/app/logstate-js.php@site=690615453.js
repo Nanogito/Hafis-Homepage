0fea6a97169c5e2a6bb1413298fafd08f16fe3d5
meisl <m.kling@sprax.org>
2018-09-24 19:08:48 +0200

    + #5: site backup @ 2018-09-24 19:03:02 ... 19:03:37 (UTC+0200) Vom Schyren-Cup 2015 und 2018 sind jetzt die großen Bilder drin. Einträge ab 04.05.2015 bis dato aufgeräumt und von Schreibfehlern befreit. Einträge zu den Büchern und zum Puppentheater verlinkt.

--- mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
+++ mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
@@ -15,7 +15,7 @@ window.diy.context.isMobilePreviewView = /mobilepreview=1/.test(location.search)
                 jQuery("<link/>", {
                     rel: "stylesheet",
                     type: "text/css",
-                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1537627828"
+                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1537808571"
                 }).appendTo("head");
                 jQuery.getScript(webPath + 'cc/logincallout.php');
             }
