b7007558fa111921923eb231fcf7fc2e65fab9f1
meisl <m.kling@sprax.org>
2018-10-06 17:49:23 +0200

    + #6: site backup @ 2018-10-06 16:17:30 ... 16:18:03 (UTC+0200) + mehr Text auf "Mein fabelhaftes Deutschheft" + paar Sachen auf "Aktuell"

--- mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
+++ mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
@@ -15,7 +15,7 @@ window.diy.context.isMobilePreviewView = /mobilepreview=1/.test(location.search)
                 jQuery("<link/>", {
                     rel: "stylesheet",
                     type: "text/css",
-                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1538164336"
+                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1538835442"
                 }).appendTo("head");
                 jQuery.getScript(webPath + 'cc/logincallout.php');
             }
