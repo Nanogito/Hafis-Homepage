cca2561b7c1f6add00b64ccb1a867c617a13277d
meisl <m.kling@sprax.org>
2019-09-12 20:10:19 +0200

    + #38 site backup @ 2019-09-12 20:00:21 ... 20:00:49 (UTC+0200)

--- mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
+++ mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
@@ -15,7 +15,7 @@ window.diy.context.isMobilePreviewView = /mobilepreview=1/.test(location.search)
                 jQuery("<link/>", {
                     rel: "stylesheet",
                     type: "text/css",
-                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1543611538"
+                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1568311217"
                 }).appendTo("head");
                 jQuery.getScript(webPath + 'cc/logincallout.php');
             }
