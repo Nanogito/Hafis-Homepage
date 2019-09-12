e790bcd3e6e181049a7930f556cc2b71de85a973
meisl <m.kling@sprax.org>
2019-09-12 22:32:34 +0200

    + #38 site backup @ 2019-09-12 22:26:10 ... 22:26:36 (UTC+0200): "Aktuell" aktualisiert mit Schyren-Cup 2019

--- mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
+++ mirror-wget/mirror/123.sb.mywebsite-editor.com/app/logstate-js.php@site=690615453.js
@@ -15,7 +15,7 @@ window.diy.context.isMobilePreviewView = /mobilepreview=1/.test(location.search)
                 jQuery("<link/>", {
                     rel: "stylesheet",
                     type: "text/css",
-                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1568311217"
+                    href: jimdoData.cdnUrl + "s/css/web/logincallout.css?1568319967"
                 }).appendTo("head");
                 jQuery.getScript(webPath + 'cc/logincallout.php');
             }
